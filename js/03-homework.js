fa02c370a49ea103536dbaa0f15d2638fd14bde5
Laimonas Remeikis <laimonasremeikis@icloud.com>
2022-01-18 22:25:30 +0200

    nd

--- js/03-homework.js
+++ js/03-homework.js
@@ -1,12 +1,11 @@
+ 
+
+
+
+
+
 // Susumuoti visus skaičiaus tipo kintamuosius
 // Rezultatą išvesti į console
-// Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
-// Rezultatą išvesti į console
-// Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
-// 1-2+3-4+5
-// Rezultatą išvesti į console
-// Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas
-
 const a1 = 2;
 const a2 = -3;
 const a3 = 8;
@@ -14,9 +13,16 @@ const a3 = 8;
 const suma = a1 + a2 + a3;
 console.log(suma)
 
+// Sujungti visus teksto tipo kintamuosius taip, jog tarp jų būtų sudarytas tarpas
+// Rezultatą išvesti į console
+
 const  t1 = 'league';
 const  t2 = 'of';
 const  t3 = 'legends';
+
+// Apskaičiuoti vertę iš sąrašų kurių verčių tipas yra skaičiai, pagal pateiktą logiką
+// 1-2+3-4+5
+// Rezultatą išvesti į console
 
 const sakinys = `${t1} ${t2} ${t3}`;
 console.log(sakinys);
@@ -27,6 +33,8 @@ console.log(alternuojantiSuma);
 
 const d = [t1, t2, t3];
 console.log(d);
+
+//Sujungti sąrašų vertes, kurių tipas yra tekstai, nuo sąrašo galo iki pradžios taip, jog tarp jų būtų kablelis ir tarpas 
 
 const sakinys1 = `${d[2]}, ${d[1]} , ${d[0]}`;
 console.log(sakinys1)
@@ -40,48 +48,45 @@ console.log(sakinys3);
 
 console.log('----------------');
 
-const pazymiai = [10, 2, 8, 6, 4];
-console.log('pazymiai:' + pazymiai);
-
-const pazymai=[10,2,8,6,4]
-let p1 = 10;
-const a=1;
-let p2 = 2;
-const b=1;
-let p3 = 8;
-const c=1;
-let p4 = 6;
-const d=1;
-let p5 = 4;
-const e=1;
-
-const suma =  p1 + p2 + p3 + p4 + p5;
-console.log('Pazymiu suma: ' + (p1+p2+p3+p4+p5));
-console.log('Pazymiu kiekis: ' + (a+b+c+d+e));
-console.log('Pazymiu vidurkis: ' + ((p1+p2+p3+p4+p5)/5));
-
-
 // Pazymiai: 10, 2, 8, 6, 4.
 // Pazymiu suma: 30.
 // Pazymiu kiekis: 5.
 // Pazymiu vidurkis: 6.
 
+const pazymiai = [10, 2, 8, 6, 4];
+console.log('pazymiai:' + pazymiai);
+
+const visiPazymiai = pazymiai[0] + `,` + pazymiai[1] + `,` + pazymiai[2] + `,` + pazymiai[3] + `,` + pazymiai[4] + `.`;
+console.log(`Pazymiai:`, visiPazymiai)
+
+let pazymiuKiekis = pazymiai.length
+console.log(`Pazymiu kiekis:`, pazymiuKiekis)
+
+let pazymiuSuma = pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3] + pazymiai[4];
+console.log(`Pazymiu suma:`, pazymiuSuma);
+
+let pazymiuVidurkis = (pazymiai[0] + pazymiai[1] + pazymiai[2] + pazymiai[3] + pazymiai[4]) / 5;
+console.log(`Pazymiu vidurkis:`, pazymiuVidurkis);
+ 
 
 console.log('----------------');
 
-
-const asmuo1 = 'Petras';
-const asmuo2 = 'Ona';
-const asmuo3 = 'Jonas';
-const asmuo4 = 'Maryte';
-
-const asmenys = [asmuo1, asmuo2, asmuo3, asmuo4];
-console.log(asmenys);
+ 
 
 // Klaseje mokosi 4 asmenys.
 // Mokiniu vardai yra Petras, Ona, Jonas, Maryte.
 
-const sakinys1 = `Klaseje mokosi 4 asmenys.`
-console.log(sakinys1)
-const sakinys2 = `Mokiniu vardai yra Petras, Ona, Jonas, Maryte.`
-console.log(sakinys2)
+const asmuo1 = `Petras`;
+const asmuo2 = `Ona`;
+const asmuo3 = `Jonas`;
+const asmuo4 = `Maryte`;
+
+const asmenys = [asmuo1, asmuo2, asmuo3, asmuo4]  
+console.log(asmenys);
+
+let mokiniuVnt = asmenys.length
+const mokiniuSkaicius = `Klaseje mokosi ${mokiniuVnt} asmenys.`;
+console.log(mokiniuSkaicius)
+
+const mokiniuVardai = `Mokiniu vardai yra ${asmuo1}, ${asmuo2}, ${asmuo3}, ${asmuo4}.`;
+console.log(mokiniuVardai)
